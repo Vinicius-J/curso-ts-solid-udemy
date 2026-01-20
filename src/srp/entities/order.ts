bf5b4fe21bd7f10edd4cc646b8258d6bcf75c494
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-01-20 08:42:32 -0300

    Single Responsibility Principle

---
+++ src/srp/entities/order.ts
@@ -0,0 +1,39 @@
+import type { OrderStatus } from './interfaces/order-status';
+import type { Messaging } from './services/messaging';
+import type { Persistency } from './services/persistency';
+import type { ShoppingCart } from './shopping-cart';
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open';
+  private readonly cart;
+  private readonly messaging;
+  private readonly persistency;
+
+  constructor(
+    cart: ShoppingCart,
+    messaging: Messaging,
+    persistency: Persistency,
+  ) {
+    this.cart = cart;
+    this.messaging = messaging;
+    this.persistency = persistency;
+  }
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.messaging.sendMessage(
+      `Seu pedido com total de ${this.cart.total()} foi recebido.`,
+    );
+    this.persistency.saveOrder();
+    this.cart.clear();
+  }
+}
---
+++ src/srp/main.ts
@@ -0,0 +1,19 @@
+import { Messaging } from './entities/services/messaging';
+import { Order } from './entities/order';
+import { Persistency } from './entities/services/persistency';
+import { Product } from './entities/product';
+import { ShoppingCart } from './entities/shopping-cart';
+
+const shoppingCart = new ShoppingCart();
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9123));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+order.checkout();
+console.log(order.orderStatus);
